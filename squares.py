11397e45e0e49bcb9926a35ffa75513be091a982
Shivani Gautam <shivani.gautam@mnsu.edu>
2020-03-24 15:39:32 -0500

    Add files via upload

---
+++ squares.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+    
+print(squares)
+
+
+# In[3]:
+
+
+squares = []
+for value in range(1,101):
+    square = value ** 2
+    squares.append(square)
+
+print(squares)
+
